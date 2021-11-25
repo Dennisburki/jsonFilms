8281990d0a22ffb6d547719af15593668878fab6
Dennis <basketor30@hotmail.fr>
2021-11-25 13:53:09 +0100

    last commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,53 @@
 //Pour utiliser fetch le live server doit etre actif - pour ca il faut ouvrir le dossier qui contient tous les ficheirs
 
+function getInfo(){
+    let toto = document.getElementById("mySearch").value
+
+    document.getElementById("copy").innerHTML =""
+
+
+fetch(`https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${toto}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+		"x-rapidapi-key": "9df895f41emshe653ddb950d0a94p1784e2jsncf90d4ad6c0f"
+	}
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    console.log(data.titles[0].title)
+
+    data.titles.forEach(element => {
+
+    document.getElementById("theSearch").innerHTML = `<div class="col-lg-3 col-8" id="theSearch">
+
+        <div class="row m-auto" id="${element.id}">
+
+            <img src ="${element.image}" class="card col-lg-5 mt-3">
+
+            <div class="col-lg-7">
+
+                <div class="row mt-3">
+
+                    <p class="col-lg-12 fw-bold">${element.title}</p>
+                    <p class="text col-lg-12">${element.overview}</p>
+                    <p class = "text-warning">${displayStar(element.vote_average)}</p>
+
+                </div>
+            </div>
+        </div>
+    </div>`
+})
+})
+}
+
 fetch("assets/json/movies.json")
 .then(response => response.json())
 .then(data => {
     // console.log(data.results[0].original_title) // pour l'original title de l'objet a l'index 0
 
     data.results.forEach(element => {
-
-        // let myId = element.id
-        // let originalTitle = element.original_title
-        // let overview = element.overview
-        // let posterPath = element.poster_path
-        // let voteAverage = element.vote_average
 
         document.getElementById("copy").innerHTML += `<div class="col-lg-3 col-8" id="copy">
 
@@ -33,10 +69,10 @@ fetch("assets/json/movies.json")
     </div>`
 
         // console.log(element.original_title) // pour tous les original title du fichier json  
-    });
-
+    })
 
 })
+
 
 function displayStar(note){
     let vote_average = Number(note) / 2
@@ -48,31 +84,27 @@ function displayStar(note){
     switch(vote_average){
 
         case 1 :
-            stars = `<i class="bi bi-star-fill"></i>`
+            stars = `<i class="bi bi-tv-fill"></i> `
             break;
 
         case 2 :
-            stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`
+            stars = `<i class="bi bi-tv-fill"></i> <i class="bi bi-tv-fill"></i> `
             break;
 
         case 3 :
-            stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`
+            stars = `<i class="bi bi-tv-fill"></i> <i class="bi bi-tv-fill"> </i><i class="bi bi-tv-fill"></i>`
             break;
 
         case 4 :
-            stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`
+            stars = `<i class="bi bi-tv-fill"></i> <i class="bi bi-tv-fill"></i> <i class="bi bi-tv-fill"></i> <i class="bi bi-tv-fill"></i>`
             break;
 
         case 5 :
-            stars = `<i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>`
+            stars = `<i class="bi bi-tv-fill"></i><i class="bi bi-tv-fill"></i><i class="bi bi-tv-fill"></i><i class="bi bi-tv-fill"></i><i class="bi bi-tv-fill"></i>`
             break;
 
         default:
-            break;
-        
+            break;   
     }
-
     return stars
 }
-
-
